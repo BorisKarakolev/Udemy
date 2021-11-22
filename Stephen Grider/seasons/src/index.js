8677dd4b26823236e4d73065740d92e4ad02cb24
BorisKarakolev <boriskarakolev@hotmail.com>
2021-11-23 00:05:10 +0100

    lifecycle methods

--- Stephen Grider/seasons/src/index.js
+++ Stephen Grider/seasons/src/index.js
@@ -1,44 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// const App = () => {
-//   return <div>Latitude: </div>;
-// };
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { latitude: null, longitude: null, error: "" };
 
-    // This is the only time we do direct assignment to this.state.
-    this.state = { latitude: null, longitude: null, error: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // To update state object we called setState, which is func. called automatically with React.Component.
-        this.setState({ latitude: position.coords.latitude });
-        this.setState({ longitude: position.coords.longitude });
+      (position) => {this.setState({ latitude: position.coords.latitude });
       },
-      (err) => {
-        this.setState({ error: err.message });
-      }
+      (err) => this.setState({ error: err.message })
     );
   }
 
   // React says we have to define render!!
   render() {
-    if (this.state.error && !this.state.latitude && !this.state.longitude) {
+    if (this.state.error && !this.state.latitude) {
       return <div>Error: {this.state.error}</div>;
     }
 
-    if (!this.state.error && this.state.latitude && this.state.longitude) {
-      return (
-        <div>
-          <div>Latitude: {this.state.latitude}</div>
-          <div>Longitude: {this.state.longitude}</div>
-        </div>
-      );
+    if (!this.state.error && this.state.latitude) {
+      return <SeasonDisplay latitude={this.state.latitude} />;
     }
-    return <h1>Loading...</h1>
+    return <Loader message='Please accept location request'/>;
   }
 }
 

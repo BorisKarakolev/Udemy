d5bd918754aa69a9fa84295db34cd6be57799d8b
BorisKarakolev <boriskarakolev@hotmail.com>
2021-12-09 16:02:13 +0100

    Using hooks

--- Stephen Grider/widgets/src/components/App.js
+++ Stephen Grider/widgets/src/components/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import Accordion from "./Accordion";
+// import Accordion from "./Accordion";
+import Search from "./Search";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is front end javascript framework.",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite js library among engineers.",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components.",
-  },
-];
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is front end javascript framework.",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite js library among engineers.",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components.",
+//   },
+// ];
 
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };

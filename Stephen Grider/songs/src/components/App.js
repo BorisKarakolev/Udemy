7df906ba3bf5c5b303daefa308d6c86d38f37086
BorisKarakolev <boriskarakolev@hotmail.com>
2021-12-23 00:20:43 +0100

    started with Redux, actions, reducers, connecting

---
+++ Stephen Grider/songs/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SongList from './SongList';
+
+const App = () => {
+    return (
+        <h1>
+            <SongList />
+        </h1>
+    )
+}
+
+export default App

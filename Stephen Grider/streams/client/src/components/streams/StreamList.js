8f351e4c6d74dd64c61e444a4aa5eacca6be3d5b
Boris Karakolev <boriskarakolev@hotmail.com>
2022-02-06 13:50:24 +0100

    finishing the streamy app with component show stream by implementing the video

--- Stephen Grider/streams/client/src/components/streams/StreamList.js
+++ Stephen Grider/streams/client/src/components/streams/StreamList.js
@@ -24,13 +24,15 @@ class StreamList extends React.Component {
   }
 
   renderList() {
-    return this.props.streams.map((stream) => {
+    return this.props.streams.filter(stream => stream.userId === this.props.currentUserId).map((stream) => {
       return (
         <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`} className='header'>
+              {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
